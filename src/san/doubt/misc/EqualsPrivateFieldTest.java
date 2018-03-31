a5f2b98f5b25493c7d40d36c98692524a166ef29
dashsan <santdash@yahoo.com>
2018-03-31 21:30:19 +0530

    Cleaning

--- src/san/doubt/misc/EqualsPrivateFieldTest.java
+++
@@ -1,9 +0,0 @@
-package san.doubt.misc;
-
-public class EqualsPrivateFieldTest {
-    public static void main(String[] args) {
-        EqualsPrivateField ep1 = new EqualsPrivateField();
-        EqualsPrivateField ep2 = new EqualsPrivateField();
-        System.out.println(ep1.equals(ep2));
-    }
-}
